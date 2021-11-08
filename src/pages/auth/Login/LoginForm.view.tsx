5862aa392a201873a7e7b0c776d340b11f4f64a0
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2021-11-08 16:50:50 +0530

    feat: added formik form validation(basic)

--- src/pages/auth/Login/LoginForm.view.tsx
+++ src/pages/auth/Login/LoginForm.view.tsx
@@ -1,12 +1,49 @@
 import { Button, Input } from '@components';
+import { useFormik } from 'formik';
 import styles from './login.module.scss';
 
 export function LoginForm() {
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: ''
+    },
+    validate: values => {
+      const errors: any = {};
+      if (!values.email) {
+        errors.email = 'Required';
+      } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+        errors.email = 'Invalid email address';
+      }
+      if (!values.password) {
+        errors.password = 'Required';
+      }
+      return errors;
+    },
+    onSubmit: values => {
+      console.log(values);
+    }
+  });
+
   return (
-    <form className={styles.form}>
-      <Input label='Email' type='email' />
-      <Input type='password' label='Password' />
-      <Button onClick={() => {}} label='Login' />
+    <form className={styles.form} onSubmit={formik.handleSubmit}>
+      <Input
+        label='Email'
+        type='email'
+        name='email'
+        onChange={formik.handleChange}
+        value={formik.values.email}
+        tip={formik.touched.email ? formik.errors.email : ''}
+      />
+      <Input
+        type='password'
+        label='Password'
+        name='password'
+        onChange={formik.handleChange}
+        value={formik.values.password}
+        tip={formik.touched.password ? formik.errors.password : ''}
+      />
+      <Button onClick={formik.submitForm} label='Login' />
     </form>
   );
 }
